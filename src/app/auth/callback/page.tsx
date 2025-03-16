6930704277edc949201585cf8356011bbe9c3457
onjiro <onjiro.mohyahya@gmail.com>
2025-03-16 15:28:46 +0900

    クライアントコンポーネントからserver用のutilを呼んでいたのを修正

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/client";
 
 export default function AuthCallbackPage() {
   const router = useRouter();
