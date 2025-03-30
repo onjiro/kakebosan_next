6e1e502236cf2c7a28ff96207fd5bded590b60c5
onjiro <onjiro.mohyahya@gmail.com>
2025-03-30 21:57:16 +0900

    アイコンをつけた

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -4,6 +4,8 @@ import { Tables } from "@/types/supabase";
 import { useState } from "react";
 import TransactionModal from "../features/TransactionModal";
 import { useRouter } from "next/navigation";
+import { FaArrowsAltH } from "@react-icons/all-files/fa/FaArrowsAltH";
+import { FaCommentDollar } from "@react-icons/all-files/fa/FaCommentDollar";
 
 type Transaction = Tables<"accounting_transactions"> & {
   entries: (Tables<"accounting_entries"> & {
@@ -26,6 +28,10 @@ export default function TransactionCard({ transaction, items }: Props) {
   const creditEntries = transaction.entries.filter(
     (entry) => entry.side === "credit"
   );
+  const isTransfer =
+    debitEntries[0].item.accounting_type === "asset" ||
+    debitEntries[0].item.accounting_type === "liability" ||
+    debitEntries[0].item.accounting_type === "equity";
 
   return (
     <>
@@ -33,12 +39,25 @@ export default function TransactionCard({ transaction, items }: Props) {
         className="bg-(--background) rounded-lg shadow p-4 cursor-pointer hover:bg-(--foreground)/5 transition-colors duration-200 flex justify-between items-start"
         onClick={() => setIsModalOpen(true)}
       >
-        <div>
-          <div className="text-sm text-(--foreground)/60">
-            {new Date(transaction.date).toLocaleDateString("ja-JP")}
+        <div className="flex items-center gap-4">
+          <div className="text-3xl">
+            {isTransfer ? (
+              <div className="p-2 rounded-full bg-green-500/10">
+                <FaArrowsAltH className="text-green-500/50" />
+              </div>
+            ) : (
+              <div className="p-2 rounded-full bg-orange-500/10">
+                <FaCommentDollar className="text-orange-500/50" />
+              </div>
+            )}
           </div>
-          <div className="font-xl">
-            {debitEntries.map((entry) => entry.item.name).join(", ")}
+          <div>
+            <div className="text-sm text-(--foreground)/60">
+              {new Date(transaction.date).toLocaleDateString("ja-JP")}
+            </div>
+            <div className="font-xl">
+              {debitEntries.map((entry) => entry.item.name).join(", ")}
+            </div>
           </div>
         </div>
         <div className="text-right">
