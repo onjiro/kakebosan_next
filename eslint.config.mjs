003bb8ae8347b68becd8a48a71e11ccfcb154807
onjiro <onjiro.mohyahya@gmail.com>
2025-12-13 22:37:15 +0900

    Revert "npx @next/codemod@canary upgrade latest"
    This reverts commit 68443bd0c264823f0c3756e6cb25e9568616bd4e.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,16 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { updateSession } from './lib/supabase/middleware';
 
-export async function proxy(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     return await updateSession(req);
 }
 
